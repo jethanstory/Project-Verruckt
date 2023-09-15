94bf1a2bea464646ceb27ccddc0e0fbd39fae131
jethanstory <32407746+jethanstory@users.noreply.github.com>
2023-09-14 18:27:46 -0600

    Added pickup blood script and lab scene to build

--- Project-Verruckt/Assets/Scripts/LockedBiometricIDDoorCheckScr.cs
+++ Project-Verruckt/Assets/Scripts/LockedBiometricIDDoorCheckScr.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LockedBiometricIDDoorCheckScr : MonoBehaviour
 {
@@ -10,21 +11,23 @@ public class LockedBiometricIDDoorCheckScr : MonoBehaviour
     public GameObject buzzer;
     public GameObject crouchJumpText;
     public GameObject crouchJumpTrigger;
+
+    public GameObject attachedObject;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        BioCheck = attachedObject.GetComponent<BloodPickupScr>().bloodCollected;
     }
 
     private void OnTriggerEnter(Collider other) // to see when the player enters the collider
     {
-        if(other.gameObject.tag == "BioDoor") //on the object you want to pick up set the tag to be anything, in this case "object"
+        if (other.gameObject.tag == "BioDoor") //on the object you want to pick up set the tag to be anything, in this case "object"
         {
             if (BioCheck == false)
             {
@@ -32,9 +35,13 @@ public class LockedBiometricIDDoorCheckScr : MonoBehaviour
                 buzzer.SetActive(false);
                 buzzer.SetActive(true);
             }
+            if (BioCheck)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
         }
 
-        if(other.gameObject.tag == "CrouchJump") //on the object you want to pick up set the tag to be anything, in this case "object"
+        if (other.gameObject.tag == "CrouchJump") //on the object you want to pick up set the tag to be anything, in this case "object"
         {
             crouchJumpText.SetActive(true);
         }
@@ -44,6 +51,6 @@ public class LockedBiometricIDDoorCheckScr : MonoBehaviour
     {
         textShow.SetActive(false);
         crouchJumpText.SetActive(false);
-     
+
     }
 }
--- Project-Verruckt/Assets/Scripts/RotateLightScr.cs
+++ Project-Verruckt/Assets/Scripts/RotateLightScr.cs
@@ -6,6 +6,8 @@ public class RotateLightScr : MonoBehaviour
 {
     private Vector3 rotation;
     public float speed;
+
+    public bool isRandom;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,25 @@ public class RotateLightScr : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        StartCoroutine(RandomSpeed());
+
         transform.Rotate(rotation * speed * Time.deltaTime);
+    }
+    IEnumerator RandomSpeed()
+    {
+        if (isRandom)
+        {
+            //speed = Random.Range(0f, 100.0f);
+            speed = 25f;
+            yield return new WaitForSeconds(5);
+            transform.Rotate(rotation * speed * Time.deltaTime);
+            speed = 5f;
+            yield return new WaitForSeconds(5);
+            transform.Rotate(rotation * speed * Time.deltaTime);
+            // speed = Random.Range(0f, 50f);
+            // speed = 15;
+            // yield return new WaitForSeconds(5);
+            // transform.Rotate(rotation * speed * Time.deltaTime);
+        }
     }
 }
