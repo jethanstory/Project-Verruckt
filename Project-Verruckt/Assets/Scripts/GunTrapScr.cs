3128d5c85d2def1661515b2044aaa66f2575ec19
jethanstory <32407746+jethanstory@users.noreply.github.com>
2025-02-21 23:18:13 -0600

    Added basic gun trap script and minor lab changes

---
+++ Project-Verruckt/Assets/Scripts/GunTrapScr.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunTrapScr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) // to see when the player enters the collider
+    {
+        if (other.gameObject.tag == "GunBoobyTrap") //on the object you want to pick up set the tag to be anything, in this case "object"
+        {
+            
+        }
+    }
+}
