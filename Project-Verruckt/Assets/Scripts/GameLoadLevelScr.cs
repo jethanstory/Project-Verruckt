5a8c130b46fbb86489ee1b2370aafb03394c70b8
jethanstory <32407746+jethanstory@users.noreply.github.com>
2025-09-21 18:35:21 -0500

    Added level transitions and some sound effects

---
+++ Project-Verruckt/Assets/Scripts/GameLoadLevelScr.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLoadLevelScr : MonoBehaviour
+{
+    float textTime = 0f;
+    public GameObject loadingText;
+    public GameObject loadingText2;
+
+    public GameObject woodDoorOpen;
+    public GameObject woodDoorClose;
+    public bool checkEnd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        checkEnd = false;
+        //SceneManager.LoadScene("MainZone");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textTime += Time.deltaTime;
+        loadingText.SetActive(true);
+        woodDoorOpen.SetActive(true);
+
+        if (textTime > 3)
+        {
+            woodDoorClose.SetActive(true);
+        }
+
+        if (textTime > 5)
+        {
+            loadingText.SetActive(false);
+            loadingText2.SetActive(true);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            checkEnd = true;
+
+        }
+
+        if (checkEnd)
+        {
+            textTime = 0f;
+            checkEnd = false;
+        }
+
+        // if (Input.anyKey)
+        // {
+        //     loadingText2.SetActive(false);
+        //     loadingText.SetActive(false);
+        //     //SceneManager.LoadScene("TestStartScene");
+        //     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // }
+    }
+}
