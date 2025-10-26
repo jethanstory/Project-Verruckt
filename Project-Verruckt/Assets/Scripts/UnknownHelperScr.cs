e9856f0e83919ba146f9b889993147f02d246951
jethanstory <32407746+jethanstory@users.noreply.github.com>
2025-10-25 21:18:18 -0500

    Added unknown helper script and associated gameobjects

---
+++ Project-Verruckt/Assets/Scripts/UnknownHelperScr.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnknownHelperScr : MonoBehaviour
+{
+    public GameObject firstHelperItem;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other) // to see when the player enters the collider
+    {
+        if (other.gameObject.tag == "FirstHelperTrigger") //on the object you want to pick up set the tag to be anything, in this case "object"
+        {
+            firstHelperItem.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+
+    }
+}
