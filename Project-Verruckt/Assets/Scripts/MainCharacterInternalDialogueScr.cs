87e8faab210d322b6f20f36eba77a20ec4f27ee4
jethanstory <32407746+jethanstory@users.noreply.github.com>
2025-11-03 22:13:53 -0600

    Added dialogue for first time taking the pills

--- Project-Verruckt/Assets/Scripts/MainCharacterInternalDialogueScr.cs
+++ Project-Verruckt/Assets/Scripts/MainCharacterInternalDialogueScr.cs
@@ -6,9 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class MainCharacterInternalDialogueScr : MonoBehaviour
 {
-    float secondsCount = 0;
+    public float secondsCount = 0;
+    public float secondsCountPast = 0;
     string sceneName;
+    bool firstTime = true;
     public Text Txt;
+    public GameObject fpsPlayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +29,32 @@ public class MainCharacterInternalDialogueScr : MonoBehaviour
                 Txt.text = "Ah shit, I left my painkillers at home. Maybe they have some here."; // "Ron, \n \n Did you take my key? I can't find it anywhere. \n \n - Becky"
             if (secondsCount >= 9)
                 Txt.text = "";
+
+            if (firstTime && fpsPlayer.GetComponent<PickUpObject>().isViewing)
+            {
+                secondsCountPast += Time.deltaTime;
+                Txt.text = "HOLY FUCK WHAT IS GOING ON";
+                if (secondsCountPast >= 3)
+                    Txt.text = "";
+                if (secondsCountPast >= 6)
+                    Txt.text = "Fuck I shouldn't have taken those. oh god";
+                if (secondsCountPast >= 9)
+                    Txt.text = "";
+                if (secondsCountPast >= 12)
+                    Txt.text = "Oh shit man";
+                if (secondsCountPast >= 15)
+                    Txt.text = "";
+                if (secondsCountPast >= 18)
+                    Txt.text = "What the fuck";
+                if (secondsCountPast >= 21)
+                    Txt.text = "";
+            }
+            if (!firstTime && fpsPlayer.GetComponent<PickUpObject>().isViewing && secondsCountPast > 1)
+            {
+                secondsCountPast = 0;
+                firstTime = false;
+            }
+
         }
 
     }
